0012ff0cc7bef32ee4541956b131ef24cad67e7f
kgentel <katie.m.gentel@gmail.com>
2021-08-04 10:11:09 -0500

    added photolist component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+// import React, { useState } from "react";
+// import Nav from "./components/Nav";
+// import About from "./components/About";
+// import Gallery from "./components/Gallery";
+// // import './App.css';
+
+// function App() {
+//   const [categories] = useState([
+//     {
+//       name: "commercial",
+//       description:
+//         "Photos of grocery stores, food trucks, and other commercial projects",
+//     },
+//     { name: "portraits", description: "Portraits of people in my life" },
+//     { name: "food", description: "Delicious delicacies" },
+//     {
+//       name: "landscape",
+//       description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+//     },
+//   ]);
+
+//   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+//   return (
+//     <div>
+//       <Nav
+//         categories={categories}
+//         setCurrentCategory={setCurrentCategory}
+//         currentCategory={currentCategory}
+//       ></Nav>
+//       <main>
+//         <div>
+//           <Gallery currentCategory={currentCategory}></Gallery>
+//           <About></About>
+//         </div>
+//       </main>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import Gallery from "./components/Gallery";
-// import './App.css';
+import Gallery from './components/Gallery';
 
 function App() {
   const [categories] = useState([
@@ -26,10 +68,8 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
